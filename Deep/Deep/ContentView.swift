c672fc4fd2cc13c47ab7a840b9e9efd312dfb4a3
Yossa Bourne <cybercanonx@gmail.com>
2026-05-26 15:08:31 +0700

    chore: first commit

---
+++ Deep/Deep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Deep
+//
+//  Created by Yossa Bourne on 5/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Deep/Deep/DeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepApp.swift
+//  Deep
+//
+//  Created by Yossa Bourne on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct DeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
